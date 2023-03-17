9c87601e742519f36dec3ee3607ca478930047c9
Mosc <daniel.moscoviter@gmail.com>
2023-03-17 23:14:51 +0100

    Make example app non-awful

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:flutter/widgets.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart' as intl;
 import 'package:relative_time/relative_time.dart';
 
-void main() {
+Future<void> main() async {
+  await initializeDateFormatting();
   runApp(const RelativeTimeExampleApp());
 }
 
@@ -12,26 +17,155 @@ class RelativeTimeExampleApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final DateTime now = DateTime.now();
-    final DateTime past = now.subtract(const Duration(days: 3, hours: 22));
-    final DateTime future = now.add(const Duration(days: 31, minutes: 54));
-
-    return WidgetsApp(
-      builder: (BuildContext context, _) => Center(
-        child: Column(
-          children: <Widget>[
-            Text(RelativeTime(context).format(past)),
-            Text(future.relativeTime(context)),
-          ],
-        ),
-      ),
-      color: const Color(0xFFFFFFFF),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
       localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
         AppLocalizations.delegate,
         RelativeTimeLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  _HomeState()
+      : selectedDate = DateTime.now(),
+        timeUnits = TimeUnit.values.toSet(),
+        numeric = false {
+    selectedTime = TimeOfDay.fromDateTime(selectedDate);
+  }
+
+  DateTime selectedDate;
+  late TimeOfDay selectedTime;
+  Set<TimeUnit> timeUnits;
+  bool numeric;
+
+  DateTime get selectedDateTime => DateTime(
+        selectedDate.year,
+        selectedDate.month,
+        selectedDate.day,
+        selectedTime.hour,
+        selectedTime.minute,
+      );
+
+  @override
+  void initState() {
+    Timer.periodic(const Duration(seconds: 1), (_) => setState(() {}));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(AppLocalizations.of(context).appName)),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: <Widget>[
+          Wrap(
+            spacing: 16,
+            runSpacing: 16,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () async {
+                  final newValue = await showDatePicker(
+                    context: context,
+                    initialDate: selectedDate,
+                    firstDate: DateTime(-271820),
+                    lastDate: DateTime(275760),
+                  );
+
+                  if (newValue != null) {
+                    setState(() => selectedDate = newValue);
+                  }
+                },
+                child: Text(AppLocalizations.of(context).pickDate),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final newValue = await showTimePicker(
+                    context: context,
+                    initialTime: selectedTime,
+                  );
+
+                  if (newValue != null) {
+                    setState(() => selectedTime = newValue);
+                  }
+                },
+                child: Text(AppLocalizations.of(context).pickTime),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          CheckboxListTile(
+            value: numeric,
+            onChanged: (bool? newValue) {
+              if (newValue != null) {
+                setState(() => numeric = newValue);
+              }
+            },
+            title: Text(AppLocalizations.of(context).numeric),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsetsDirectional.only(start: 16),
+            child: Text(
+              AppLocalizations.of(context).timeUnits,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          const SizedBox(height: 16),
+          for (final TimeUnit timeUnit in TimeUnit.values)
+            Padding(
+              padding: const EdgeInsetsDirectional.only(start: 16),
+              child: CheckboxListTile(
+                value: timeUnits.contains(timeUnit),
+                onChanged: (bool? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      if (newValue) {
+                        timeUnits.add(timeUnit);
+                      } else {
+                        timeUnits.remove(timeUnit);
+                      }
+                    });
+                  }
+                },
+                title: Text(intl.toBeginningOfSentenceCase(timeUnit.name)!),
+              ),
+            ),
+        ],
+      ),
+      bottomNavigationBar: ColoredBox(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              AppLocalizations.of(context).formatsTo(
+                selectedDateTime,
+                selectedDateTime,
+                selectedDateTime.relativeTime(
+                  context,
+                  timeUnits: timeUnits,
+                  numeric: numeric,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
